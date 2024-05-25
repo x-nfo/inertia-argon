f8a103624ed9e495c0c6e867a564a90e98c53b0f
x-info <dmsadikins@gmail.com>
2024-05-25 15:47:31 +0700

    update delete confirmation

--- resources/js/Pages/Users/DeleteBulk.vue
+++ resources/js/Pages/Users/DeleteBulk.vue
@@ -54,13 +54,13 @@ const closeModal = () => {
                 <h2
                     class="text-lg font-medium text-gray-900 dark:text-slate-200"
                 >
-                    Are you sure you want to delete your account?
+                    Are you sure you want to delete?
                 </h2>
 
                 <p class="mt-1 text-sm text-gray-600 dark:text-slate-200">
-                    Once your account is deleted, all of its resources and data
+                    Once your data is deleted, all of its resources and data
                     will be permanently deleted. Please enter your password to
-                    confirm you would like to permanently delete your account.
+                    confirm you would like to permanently delete.
                 </p>
 
                 <div class="mt-6">
